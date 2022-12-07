b506e22bf6ee0fc2446ae2f698738ba43a8d90fa
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-07 09:35:31 -0800

    create student component setup

--- src/components/create-student.jsx
+++ src/components/create-student.jsx
@@ -1,2 +1,11 @@
 import { Component } from "react";
 
+export default class CreateStudent extends Component {
+    render() {
+        return (
+            <div>
+                <p>Create Student Component here</p>
+            </div>
+        )
+    }
+}
