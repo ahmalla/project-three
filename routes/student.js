b1f2112be8503e7ddcebfc5314dfc64c6e8547d1
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-07 11:15:08 -0800

    update student

--- routes/student.js
+++ routes/student.js
@@ -35,4 +35,19 @@ router.route('/edit-student/:id').get((req, res) => {
       }
     })
   })
+
+  // Update Student
+router.route('/update-student/:id').put((req, res, next) => {
+    studentSchema.findByIdAndUpdate(req.params.id, {
+      $set: req.body
+    }, (error, data) => {
+      if (error) {
+        return next(error);
+        console.log(error)
+      } else {
+        res.json(data)
+        console.log('Student updated successfully !')
+      }
+    })
+  })
   
