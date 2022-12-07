8c3bfb753dc5766def4c30a458a503d7ee8a7eda
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-07 11:30:07 -0800

    delete student

--- routes/student.js
+++ routes/student.js
@@ -50,4 +50,18 @@ router.route('/update-student/:id').put((req, res, next) => {
       }
     })
   })
+
+  // Delete Student
+router.route('/delete-student/:id').delete((req, res, next) => {
+    studentSchema.findByIdAndRemove(req.params.id, (error, data) => {
+      if (error) {
+        return next(error);
+      } else {
+        res.status(200).json({
+          msg: data
+        })
+      }
+    })
+  })
+  module.exports = router;
   
