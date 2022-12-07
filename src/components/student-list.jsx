e54a57491b0c1b5f43ba708e518fe809bea0d61c
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-07 09:39:15 -0800

    studentlist component setup

---
+++ src/components/student-list.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class StudentList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Student List Component</p>
+            </div>
+        )
+    }
+}
