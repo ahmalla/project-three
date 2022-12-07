e08bcc6f1d8f6d34997be5e3c9fde6708c0c7438
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-07 10:02:08 -0800

    app.js setup with imports and bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App/App';
+import * as serviceWorker from './pages/service/serviceWorker'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,3 +13,4 @@ root.render(
 );
 
 
+serviceWorker.unregister();
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,25 +1,76 @@
+import  Nav  from "react-bootstrap";
+import  Navbar  from "react-bootstrap";
+import Container from "react-bootstrap";
+import Row from "react-bootstrap";
+import Col from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css"
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CreateStudent from "../../components/create-student";
+import EditStudent from "../../components/edit-student";
+import StudentList from "../../components/student-list";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to={'/create-student'} className="nav-link">
+                  StudentTracker
+                </Link>
+              </Navbar.Brand>
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={'/create-student'} className="nav-link">
+                    Create Student
+                  </Link>
+                </Nav>
+                <Nav>
+                  <Link to={'/student-list'} className="nav-link">
+                    Student List
+                  </Link>
+                </Nav>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <Container>
+          <Row>
+            <Col md={12}>
+              <div className="wrapper">
+                <Switch>
+                  <Route
+                    exact
+                    path="/"
+                    component={(props) => <CreateStudent {...props} />}
+                  />
+                  <Route
+                    exact
+                    path="/create-student"
+                    component={(props) => <CreateStudent {...props} />}
+                  />
+                  <Route
+                    exact
+                    path="/edit-student/:id"
+                    component={(props) => <EditStudent {...props} />}
+                  />
+                  <Route
+                    exact
+                    path="/student-list"
+                    component={(props) => <StudentList {...props} />}
+                  />
+                </Switch>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
